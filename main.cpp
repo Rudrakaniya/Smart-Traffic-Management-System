aae8f3bb3792e65ece9d7be920372b2618747319
RUDRA KANIYA <41951699+Rudrakaniya@users.noreply.github.com>
2019-03-23 16:03:46 +0530

    main.cpp
    Framework of the project.

---
+++ main.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+#include <fstream>
+// ***** Smart Traffic Management Solution *****
+
+//keep the record of vehicles                      == recOfVeh()
+//keep the record of challan done                  == recOfChall()
+//Search the record of vehicles                    == vehSearch()
+//Display information of traffic in control booths == trafContBooth()
+//Helpline Information and nearby hospitals        == helpInfo()
+//control the traffic                              == trafCont()
+
+
+using namespace std;
+
+class SmartTrafficManagementSystem{
+
+
+public:
+
+    int recOfVeh()
+    {
+
+        return 0;
+    }
+
+    int recOfChall()
+    {
+
+        return  0;
+    }
+
+    int vehSearch()
+    {
+
+        return 0;
+    }
+
+    int trafContBooth()
+    {
+
+        return 0;
+    }
+
+    int helpInfo()
+    {
+
+        return 0;
+    }
+    int trafCont()
+    {
+
+        return 0;
+    }
+
+
+
+};
+int main()
+{
+    cout<<">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"<<endl;
+    cout<<"*                        WELCOME TO                         *"<<endl;
+    cout<<"*                                                           *"<<endl;
+    cout<<"*              SMART TRAFFIC MANAGEMENT SYSTEM              *"<<endl;
+    cout<<"*                                                           *"<<endl;
+    cout<<"*                                                           *"<<endl;
+    cout<<"*  Press Your Option :-                                     *"<<endl;
+    cout<<"*                                                           *"<<endl;
+    cout<<"*        1.Record of Vehicles                               *"<<endl;
+    cout<<"*        2.Record of Challan                                *"<<endl;
+    cout<<"*        3.Search the Record of Vehicles                    *"<<endl;
+    cout<<"*        4.Traffic Control Booths                           *"<<endl;
+    cout<<"*        5.Control the Traffic                              *"<<endl;
+    cout<<"*        6.Help !                                           *"<<endl;
+    cout<<"*                                                           *"<<endl;
+    cout<<"*                                                           *"<<endl;
+    cout<<"*                                                           *"<<endl;
+    cout<<">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"<<endl;
+
+
+
+//    cout<<"           WELCOME TO \n";
+//    cout<<"SMART TRAFFIC MANAGEMENT SYSTEM"<<endl;
+}
